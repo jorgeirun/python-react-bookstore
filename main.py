942c7465e551851f282b633fe8a83317327e10b5
Jorge <jorgemiv2@gmail.com>
2024-07-25 16:43:35 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from typing import Union
+from fastapi import FastAPI
+import json
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/books/")
+# http://127.0.0.1:8000/books/?skip=0&limit=6
+# skip and limit are optional
+def get_books(skip: int = 0, limit: int = 6):
+    with open('books.json') as f:
+        d = json.load(f)
+        return {"data": d[skip : skip + limit]}
